499fd331d13204c73bc23298dd4703336c47bed3
arsenal28 <arsenal28@gmail.com>
2018-02-26 14:02:35 +0800

    2rd

--- bc_tool.py
+++ bc_tool.py
@@ -9,6 +9,7 @@ data={
     "verifySMSVerifyCode":"",
     "authCode":"AA18B9EE69F51D49F0AE8045CDA69FB6"
     }
-r=requests.get(login_url)
-response=s.post(login_url,data=json.dumps(data))
+#r=requests.get(login_url)
+headers = {'content-type': 'application/json'}  # payload请求方式
+response=s.post(login_url,data=json.dumps(data),headers=headers)
 print(response.text)
